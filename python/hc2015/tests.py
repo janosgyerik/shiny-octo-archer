06821eec2087f5bf7f48a6cf1483ceafd45c320a
Janos Gyerik <janos@kronos>
2015-03-13 08:08:44 +0100

    added more tests for get_available_slot

--- python/hc2015/tests.py
+++ python/hc2015/tests.py
@@ -20,8 +20,12 @@ class TestSolver(unittest.TestCase):
     def test_get_available_slot(self):
         row = solver.Row(0, 8)
         row.mark_unavailable(0, 1)
-        row.mark_unavailable(2, 1)
-        self.assertEquals(3, row.get_available_slot(4))
+        row.mark_unavailable(3, 1)
+        self.assertEquals(4, row.get_available_slot(4))
+        self.assertEquals(1, row.get_available_slot(1))
+        self.assertEquals(1, row.get_available_slot(2))
+        self.assertEquals(4, row.get_available_slot(3))
+        self.assertEquals(None, row.get_available_slot(5))
 
 
 if __name__ == '__main__':
