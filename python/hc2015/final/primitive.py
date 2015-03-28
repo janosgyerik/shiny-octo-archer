91fdf425ac4c565827fad536d0ff32c41ea01cc1
Janos Gyerik <janos@kronos>
2015-03-28 12:36:13 +0100

    bugfix

--- python/hc2015/final/primitive.py
+++ python/hc2015/final/primitive.py
@@ -7,8 +7,7 @@ with open('inputs/large.txt') as fh:
     _, _, A = to_int(*fh.readline().split(' '))
     _, _, B, T = to_int(*fh.readline().split(' '))
 
-for _ in range(B):
-    print(1)
+print(' '.join(['1' for _ in range(B)]))
 
 for _ in range(T - 1):
-    print(0)
+    print(' '.join(['0' for _ in range(B)]))
